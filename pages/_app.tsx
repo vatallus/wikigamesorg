03b9c3c56da587e7763a9302de35a92299d07923
hacuong <14039266+vatallus@users.noreply.github.com>
2025-12-30 12:22:39 +0700

    Add Google Analytics (G-X3LB3EHFG5) and Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { polyfill } from "seamless-scroll-polyfill";
+import { Analytics } from "@vercel/analytics/next";
 import "../styles/globals.scss";
 
 function App({ Component, pageProps }: AppProps) {
@@ -8,7 +9,12 @@ function App({ Component, pageProps }: AppProps) {
     polyfill();
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-X3LB3EHFG5"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X3LB3EHFG5');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
